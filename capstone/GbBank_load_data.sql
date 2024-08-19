d7bad87e92b33d59f557d090db7265b50e01a907
Saurabh P <47257431+saurabhp31@users.noreply.github.com>
2024-08-20 01:14:05 +0530

    Update GbBank_load_data.sql

--- capstone/GbBank_load_data.sql
+++ capstone/GbBank_load_data.sql
@@ -8,7 +8,8 @@ use database GBSTAGE_DB;
 create or replace schema GBSTAGE_DB.GBSTAGE;
 use schema GBSTAGE;
  
------- create external storage integration --------
+--------------------create external storage integration --------
+----------------------**********ALready created************----------------------------
 CREATE OR REPLACE STORAGE INTEGRATION capstone_csv
 TYPE = EXTERNAL_STAGE
 STORAGE_PROVIDER = S3
@@ -34,7 +35,7 @@ file_format = csv_file_format;
  
  
 ------ creating the  stage tables ---------------------
-CREATE TABLE GBSTAGE_DB.GBSTAGE.transactions (
+CREATE or replace TABLE GBSTAGE_DB.GBSTAGE.transactions (
     transaction_id STRING,
     customer_id STRING,
     transaction_date TIMESTAMP_NTZ,
@@ -49,7 +50,7 @@ CREATE TABLE GBSTAGE_DB.GBSTAGE.transactions (
     is_flagged BOOLEAN
 );
  
-CREATE TABLE GBSTAGE_DB.GBSTAGE.customers (
+CREATE or replace TABLE GBSTAGE_DB.GBSTAGE.customers (
     customer_id STRING,
     first_name STRING,
     last_name STRING,
@@ -65,7 +66,7 @@ CREATE TABLE GBSTAGE_DB.GBSTAGE.customers (
     customer_since DATE
 );
  
-CREATE TABLE GBSTAGE_DB.GBSTAGE.accounts (
+CREATE or replace TABLE GBSTAGE_DB.GBSTAGE.accounts (
     account_id STRING,
     customer_id STRING,
     account_type STRING,
@@ -76,7 +77,7 @@ CREATE TABLE GBSTAGE_DB.GBSTAGE.accounts (
     credit_limit FLOAT
 );
  
-CREATE TABLE GBSTAGE_DB.GBSTAGE.credit_data (
+CREATE or replace  TABLE GBSTAGE_DB.GBSTAGE.credit_data (
     customer_id STRING,
     credit_score INT,
     number_of_credit_accounts INT,
@@ -86,7 +87,7 @@ CREATE TABLE GBSTAGE_DB.GBSTAGE.credit_data (
     bankruptcies INT
 );
  
-CREATE TABLE GBSTAGE_DB.GBSTAGE.watchlist (
+CREATE or replace TABLE GBSTAGE_DB.GBSTAGE.watchlist (
     entity_id STRING,
     entity_name STRING,
     entity_type STRING,
@@ -153,6 +154,14 @@ ON_ERROR = CONTINUE;
 desc pipe watchlist_pipe;
 
 show pipes;
+
+
+ --- check data in stream and stage table-------------------- 
+select * from GBSTAGE_DB.GBSTAGE.transactions;
+select * from GBSTAGE_DB.GBSTAGE.customers;
+select * from GBSTAGE_DB.GBSTAGE.accounts;
+select * from GBSTAGE_DB.GBSTAGE.credit_data;
+select * from GBSTAGE_DB.GBSTAGE.watchlist;
  
 ------- to refresh pipes ---------
 alter pipe transactions_pipe refresh;
@@ -160,6 +169,12 @@ alter pipe customers_pipe refresh;
 alter pipe accounts_pipe refresh;
 alter pipe credit_data_pipe refresh;
 alter pipe watchlist_pipe refresh;
+
+select system$pipe_status('customers_pipe');
+
+select * from table (information_schema.
+copy_history(table_name=>'GBSTAGE_DB.GBSTAGE.customers',
+start_time=>dateadd(hours,-1, current_timestamp())));
 
  --- check data in stream and stage table-------------------- 
 select * from GBSTAGE_DB.GBSTAGE.transactions;
@@ -242,22 +257,40 @@ CREATE TABLE GBLOAD_DB.GBLOAD.watchlist (
     source STRING
 );
 
+
 ------ create task to load data from stream ---------------
+
+SELECT listagg(column_name,', ')
+    FROM INFORMATION_SCHEMA.COLUMNS where  table_schema='GBSTAGE'and table_name='TRANSACTIONS';
+
 CREATE OR REPLACE TASK GBLOAD_DB.GBLOAD.transactions_tsk
 WAREHOUSE = compute_wh 
 SCHEDULE  = '1 minute'
 WHEN system$stream_has_data('GBSTAGE_DB.GBSTAGE.transactions_stream')
 AS
 INSERT INTO GBLOAD_DB.GBLOAD.transactions
-SELECT * FROM GBSTAGE_DB.GBSTAGE.transactions_stream;
+    (IS_FLAGGED, TRANSACTION_ID, CUSTOMER_ID, TRANSACTION_DATE, AMOUNT, MERCHANT_NAME, TRANSACTION_TYPE, CURRENCY, MERCHANT_CATEGORY, LOCATION_CITY, CHANNEL, LOCATION_COUNTRY)
+SELECT IS_FLAGGED, TRANSACTION_ID, CUSTOMER_ID, TRANSACTION_DATE, AMOUNT, MERCHANT_NAME, TRANSACTION_TYPE, CURRENCY, MERCHANT_CATEGORY, LOCATION_CITY, CHANNEL, LOCATION_COUNTRY
+FROM GBSTAGE_DB.GBSTAGE.transactions_stream;
 
-CREATE OR REPLACE TASK GBLOAD_DB.GBLOAD.accounts_tsk
+
+
+SELECT listagg(column_name,', ')
+    FROM INFORMATION_SCHEMA.COLUMNS where  table_schema='GBSTAGE'and table_name='ACCOUNTS';
+
+CREATE OR REPLACE TASK GBLOAD_DB.GBLOAD.ACCOUNTS_tsk
 WAREHOUSE = compute_wh 
 SCHEDULE  = '1 minute'
-WHEN system$stream_has_data('GBSTAGE_DB.GBSTAGE.accounts_stream')
+WHEN system$stream_has_data('GBSTAGE_DB.GBSTAGE.ACCOUNTS_STREAM')
 AS
-INSERT INTO GBLOAD_DB.GBLOAD.accounts
-SELECT * FROM GBSTAGE_DB.GBSTAGE.accounts_stream;
+INSERT INTO GBLOAD_DB.GBLOAD.ACCOUNTS
+    (OPEN_DATE, CREDIT_LIMIT, ACCOUNT_STATUS, CUSTOMER_ID, ACCOUNT_TYPE, CURRENCY, CURRENT_BALANCE, ACCOUNT_ID)
+SELECT OPEN_DATE, CREDIT_LIMIT, ACCOUNT_STATUS, CUSTOMER_ID, ACCOUNT_TYPE, CURRENCY, CURRENT_BALANCE, ACCOUNT_ID
+FROM GBSTAGE_DB.GBSTAGE.accounts_stream;
+
+
+SELECT listagg(column_name,', ')
+    FROM INFORMATION_SCHEMA.COLUMNS where  table_schema='GBSTAGE'and table_name='CREDIT_DATA';
 
 
 CREATE OR REPLACE TASK GBLOAD_DB.GBLOAD.credit_data_tsk
@@ -266,8 +299,12 @@ SCHEDULE  = '1 minute'
 WHEN system$stream_has_data('GBSTAGE_DB.GBSTAGE.credit_data_stream')
 AS
 INSERT INTO GBLOAD_DB.GBLOAD.credit_data
-SELECT * FROM GBSTAGE_DB.GBSTAGE.credit_data_stream;
+(TOTAL_CREDIT_LIMIT, NUMBER_OF_LATE_PAYMENTS, BANKRUPTCIES, NUMBER_OF_CREDIT_ACCOUNTS, CUSTOMER_ID, CREDIT_SCORE, TOTAL_CREDIT_USED)
+SELECT TOTAL_CREDIT_LIMIT, NUMBER_OF_LATE_PAYMENTS, BANKRUPTCIES, NUMBER_OF_CREDIT_ACCOUNTS, CUSTOMER_ID, CREDIT_SCORE, TOTAL_CREDIT_USED 
+FROM GBSTAGE_DB.GBSTAGE.credit_data_stream;
 
+SELECT listagg(column_name,', ')
+    FROM INFORMATION_SCHEMA.COLUMNS where  table_schema='GBSTAGE'and table_name='CUSTOMERS';
 
 CREATE OR REPLACE TASK GBLOAD_DB.GBLOAD.customers_tsk
 WAREHOUSE = compute_wh 
@@ -275,16 +312,28 @@ SCHEDULE  = '1 minute'
 WHEN system$stream_has_data('GBSTAGE_DB.GBSTAGE.customers_stream')
 AS
 INSERT INTO GBLOAD_DB.GBLOAD.customers
-SELECT * FROM GBSTAGE_DB.GBSTAGE.customers_stream;
+(DATE_OF_BIRTH, INCOME_BRACKET, CITY, CUSTOMER_ID, LAST_NAME, COUNTRY, CUSTOMER_SINCE, ADDRESS, PHONE_NUMBER, OCCUPATION, FIRST_NAME, EMAIL, GENDER)
+SELECT DATE_OF_BIRTH, INCOME_BRACKET, CITY, CUSTOMER_ID, LAST_NAME, COUNTRY, CUSTOMER_SINCE, ADDRESS, PHONE_NUMBER, OCCUPATION, FIRST_NAME, EMAIL, GENDER
+FROM GBSTAGE_DB.GBSTAGE.customers_stream;
 
+SELECT listagg(column_name,', ')
+    FROM INFORMATION_SCHEMA.COLUMNS where  table_schema='GBSTAGE'and table_name='WATCHLIST';
 
 CREATE OR REPLACE TASK GBLOAD_DB.GBLOAD.watchlist_tsk
 WAREHOUSE = compute_wh 
 SCHEDULE  = '1 minute'
 WHEN system$stream_has_data('GBSTAGE_DB.GBSTAGE.watchlist_stream')
 AS
-INSERT INTO GBLOAD_DB.GBLOAD.watchlist
-SELECT * FROM GBSTAGE_DB.GBSTAGE.watchlist_stream;
+INSERT INTO GBLOAD_DB.GBLOAD.watchlist(RISK_CATEGORY, ENTITY_ID, SOURCE, ENTITY_NAME, ENTITY_TYPE, LISTED_DATE)
+SELECT RISK_CATEGORY, ENTITY_ID, SOURCE, ENTITY_NAME, ENTITY_TYPE, LISTED_DATE 
+FROM GBSTAGE_DB.GBSTAGE.watchlist_stream;
+
+
+/*execute task GBLOAD_DB.GBLOAD.transactions_tsk;
+execute task GBLOAD_DB.GBLOAD.ACCOUNTS_tsk;
+execute task GBLOAD_DB.GBLOAD.credit_data_tsk;
+execute task GBLOAD_DB.GBLOAD.customers_tsk;
+execute task GBLOAD_DB.GBLOAD.watchlist_tsk;*/
 
 -----------resume task------------------------------
 ALTER TASK GBLOAD_DB.GBLOAD.transactions_tsk RESUME;
@@ -293,7 +342,13 @@ ALTER TASK GBLOAD_DB.GBLOAD.credit_data_tsk RESUME;
 ALTER TASK GBLOAD_DB.GBLOAD.customers_tsk RESUME;
 ALTER TASK GBLOAD_DB.GBLOAD.watchlist_tsk RESUME;
 
+ALTER TASK GBLOAD_DB.GBLOAD.transactions_tsk SUSPEND;
+ALTER TASK GBLOAD_DB.GBLOAD.accounts_tsk SUSPEND;
+ALTER TASK GBLOAD_DB.GBLOAD.credit_data_tsk SUSPEND;
+ALTER TASK GBLOAD_DB.GBLOAD.customers_tsk SUSPEND;
+ALTER TASK GBLOAD_DB.GBLOAD.watchlist_tsk SUSPEND;
 
+    
 --------check stream and load table data--------------
 select * from GBSTAGE_DB.GBSTAGE.transactions_stream;
 select * from GBSTAGE_DB.GBSTAGE.customers_stream;
@@ -310,20 +365,5 @@ select * from GBLOAD_DB.GBLOAD.watchlist;
 
 ----------------------------------------------------------------------------------------------
 
-
- 
- 
-
- 
- 
---select * from transaction ;
---select *, trans_func(amount) as risk_level from transaction;
- 
- 
-REVOKE APPLYBUDGET ON DATABASE raw_db FROM ROLE PC_DBT_ROLE;
-grant all privileges on DATABASE raw_db to role PC_DBT_ROLE;
-grant all privileges on schema RAW_schema to role PC_DBT_ROLE;
-grant select on all tables in schema RAW_schema to role PC_DBT_ROLE;
-GRANT SELECT ON FUTURE TABLES IN DATABASE raw_db TO ROLE PC_DBT_ROLE;
 
  
